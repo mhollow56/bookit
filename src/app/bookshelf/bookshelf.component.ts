44d6b1621d13420e7b9c4d55569b9c8ce2cec624
mhollow56 <mikeholloway7023@protonmail.com>
2022-10-10 20:16:16 -0500

    changes to bookit

--- src/app/bookshelf/bookshelf.component.ts
+++ src/app/bookshelf/bookshelf.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book/book.model';
+
+
 
 @Component({
   selector: 'app-bookshelf',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookshelf.component.css']
 })
 export class BookshelfComponent implements OnInit {
+  selectedBook: Book;
 
-  constructor() { }
+  constructor() {
+
+   }
 
   ngOnInit(): void {
+
   }
 
 }
